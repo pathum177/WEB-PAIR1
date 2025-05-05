27b1cba254b29831f48f61d64c994125e4c003bd
pathum177 <pathummalsara177@gmail.com>
2025-05-05 16:15:52 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "ashinsad459@gmail.com",
-  password: "MniRvj3eN-RaNkK",
+  email: "cifeg35326@bocapies.com",
+  password: "pathum12345",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
